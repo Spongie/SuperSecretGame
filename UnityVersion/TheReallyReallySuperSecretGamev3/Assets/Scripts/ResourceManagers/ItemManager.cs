7c172eb41e521620440f5b2cc3ffb8dc38c565eb
Philip Persson <kalllez123@hotmail.se>
2015-05-25 19:00:15 +0200

    lite editor fix

---
+++ UnityVersion/TheReallyReallySuperSecretGamev3/Assets/Scripts/ResourceManagers/ItemManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Assets.Scripts.Utility;
+
+public class ItemManager : MonoBehaviour
+{
+    void Start()
+    {
+        IEnumerable<string> itemFiles = Resources.LoadAll("Items\\").Cast<string>();
+
+        foreach (var itemFile in itemFiles)
+        {
+            var item = JsonConvert.DeserializeObject<Item>(itemFile);
+            AllItems.Add(item);
+        }
+    }
+
+    public List<Item> AllItems { get; set; }
+
+    public IEnumerable<Item> AllWeapons
+    {
+        get { return AllItems.Where(item => item.Slot == ItemSlot.Weapon); }
+    }
+
+}
