137643d46631d1555f9c6997952b43086d8a9982
Philip Persson <kalllez123@hotmail.se>
2015-05-30 13:36:23 +0200

    Iconmanager start och lite hero psd

--- UnityVersion/TheReallyReallySuperSecretGamev3/Assets/Scripts/ResourceManagers/IconManager.cs
+++ UnityVersion/TheReallyReallySuperSecretGamev3/Assets/Scripts/ResourceManagers/IconManager.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class IconManager : MonoBehaviour
 {
     void Start()
     {
+        var icons = Resources.LoadAll<Sprite>("Icons\\").Cast<Sprite>();
 
+        foreach (var icon in icons)
+        {
+            Icons.Add(icon.name, icon);
+        }
     }
 
+    public Dictionary<string, Sprite> Icons { get; set; }
 }
