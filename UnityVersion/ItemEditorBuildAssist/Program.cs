9d7454967f3e44d3222fbf21e3f0bc397b8b71ce
Philip Persson <kalllez123@hotmail.se>
2015-06-12 12:43:01 +0200

    Auto Editor update (endast från min dator)

---
+++ UnityVersion/ItemEditorBuildAssist/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ItemEditorBuildAssist
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine(string.Format("Moving files from {0} to {1}", args[0], args[1]));
+
+            try
+            {
+                string path = Directory.GetParent(Directory.GetParent(Directory.GetParent(args[1]).FullName).FullName).FullName + "\\ItemEditor";
+
+                Console.WriteLine("Deleting old files");
+                foreach (var file in Directory.GetFiles(path))
+                {
+                    if (Path.GetExtension(file) == ".ini")
+                        continue;
+
+                    File.Delete(file);
+                }
+
+                Console.WriteLine("Copying new files");
+                foreach (var file in Directory.GetFiles(args[0]))
+                {
+                    if (Path.GetExtension(file) == ".ini")
+                        continue;
+
+                    File.Copy(file, path + "\\" + Path.GetFileName(file));
+                }
+            }
+            catch
+            {
+                return 99;
+            }
+
+            return 0;
+        }
+    }
+}
