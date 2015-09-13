521de9bed48e1e9dac41ca42082d561e32ef81b8
Philip Persson <kalllez123@hotmail.se>
2015-09-13 23:01:39 +0200

    AI-enabler

---
+++ UnityVersion/TheReallyReallySuperSecretGamev3/Assets/Scripts/Character/AIEnabler.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Scripts.Character.Monsters;
+using UnityEngine;
+
+namespace Assets.Scripts.Character
+{
+    public class AIEnabler : MonoBehaviour
+    {
+        public GameObject Player;
+
+        void Update()
+        {
+            transform.position = Player.transform.position;
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            var monster = other.gameObject.GetComponent<Monster>();
+
+            if (monster != null)
+            {
+                other.gameObject.SetActive(true);
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D other)
+        {
+            var monster = other.gameObject.GetComponent<Monster>();
+
+            if (monster != null)
+            {
+                other.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ UnityVersion/TheReallyReallySuperSecretGamev3/Assets/Scripts/Utility/EnableCopyer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Utility
+{
+    public class EnableCopyer : MonoBehaviour
+    {
+        public GameObject Target;
+
+        void Start()
+        {
+
+        }
+
+        IEnumerator Copy()
+        {
+            while (true)
+            {
+                bool targetStatus = Target.activeSelf;
+
+                if (targetStatus != gameObject.activeSelf)
+                    gameObject.SetActive(targetStatus);
+
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
